06c1471d356d62b614dbde83aed96535e1b752fa
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-09 01:47:56 +0530

    Add posts route and EJS view for displaying user posts

--- index.js
+++ index.js
@@ -10,16 +10,38 @@ app.use(express.json());
 app.set('view engine','ejs');
 app.set('views' , path.join(__dirname,'views'));
 
-app.set(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
+
+let posts=[
+  {
+    username:'Parth Tiwari',
+    content:'This is my first post.',
+  },
+  {
+    username:'Aryan Saxena',
+    content:'This is my first post. Parth is my Father.',
+  },
+  {
+    username:'Shivansh Bajpai',
+    content:'This is my first post. Aryan is my adopted son.',
+  },
+  {
+    username:'Tinku Mishra',
+    content:'This is my first post. Parth is my original Father.',
+  },
+  {
+    username:'Manas Srivastava',
+    content:'This is my first post. I am a very chaotic person.',
+  },
+];
 
 
-app.get('/',(req,res)=>{
-  res.send('Server Working Well!')
+app.get('/posts',(req,res)=>{
+  res.render('index.ejs',{posts});
 })
 
 
 
 app.listen(PORT,()=>{
   console.log(`Server is running on PORT ${PORT}`);
-  
 })
