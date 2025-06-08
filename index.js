8a22b9857c45b53c0d494499b867f45eb04400c5
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-09 01:19:12 +0530

    Initialize Express server with basic configuration and routes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const express
+const express = require('express');
+const app= express();
+const PORT= 3002;
+const path = require('path');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.set('view engine','ejs');
+app.set('views' , path.join(__dirname,'views'));
+
+app.set(express.static(path.join(__dirname,'public')));
+
+
+app.get('/',(req,res)=>{
+  res.send('Server Working Well!')
+})
+
+
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on PORT ${PORT}`);
+  
+})
