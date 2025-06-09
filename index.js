a5cd644d9388d0a60099bfb637f91e0f0516df70
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-09 13:50:59 +0530

    done CRUD Operations

--- index.js
+++ index.js
@@ -88,6 +88,12 @@ app.get('/posts/:id/edit',(req,res)=>{
 })
 
 
+app.delete('/posts/:id',(req,res)=>{
+  let {id} = req.params;
+  posts = posts.filter((p)=> id !==p.id);
+  res.redirect('/posts')
+})
+
 
 
 
